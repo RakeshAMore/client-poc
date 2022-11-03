1c58f18177c1c885744eff8c28f15be38cdbb379
Rakeshkumar More <rakeshkumar.more@synacor.com>
2022-11-03 18:56:29 +0530

    init commit

---
+++ src/main/java/com/microservice/clientpoc/ClientPocApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.clientpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/clientpoc/ClientPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.clientpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
